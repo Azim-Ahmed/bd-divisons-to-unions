16e436c835d9e852d93f0416cc6bfe282afaaec0
Azim Uddin Ahamed <azimuahmed13@gmail.com>
2023-02-04 18:16:51 +0600

    getAllUnions , getAllDivions added functions

---
+++ index.js
@@ -0,0 +1,15 @@
+function getAllDivision() {
+    return "this is for getAllDivision"
+}
+function getAllUpazilas() {
+    return "this is for getAllUpazilas"
+}
+function getAllUnions() {
+    return "this is for getAllUnions"
+}
+
+module.exports = {
+    getAllDivision,
+    getAllUpazilas,
+    getAllUnions
+}
